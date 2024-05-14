86f08062b0fb98424e63e9a6acccad6c55709eea
Elisabeth Bönisch <elisabeth.boenisch@idiv.de>
2024-05-14 16:40:51 +0200

    temporal coverage of litterfall & NDMS species level

---
+++ 2-code/2-2-analyses/2-2-8-temporal-coverage-of-litterfall.R
@@ -0,0 +1,50 @@
+#---------------------------------------------------------------------
+# MyDiv experiment; Litterfall data March 2023 - February 2024
+# 2024-05-11
+# temporal coverage of litterfall 
+# by Elisabeth Bönisch (elisabeth.boenisch@idiv.de)
+# updated on 
+
+#============================ Packages ===============================
+
+rm(list=ls())
+
+library(readr)
+library(readxl)
+library(tidyverse)
+library(nlme)
+library(lme4)
+library(lmerTest)
+
+#============================ Dataset ===============================
+
+df.all.wide.info <- read.csv("2-1-1-Full-data-wideformat-MyDiv-litter-dryweight.csv")
+
+# 1) NO! average across traps ####
+df.all.wide.trap <- df.all.wide.info %>%
+  dplyr::rename(Ac = "Acer.pseudoplatanus",	
+                Ae = "Aesculus.hippocastanum",
+                Be = "Betula.pendula",	
+                Ca = "Carpinus.betulus",
+                Fa = "Fagus.sylvatica",	
+                Fr = "Fraxinus.excelsior",	
+                Pr = "Prunus.avium",	
+                Qu = "Quercus.petraea", 
+                So = "Sorbus.aucuparia",	
+                Ti = "Tilia.platyphyllos", 
+                cont = "contaminants")
+
+df.all.wide.trap$div<-as.factor(df.all.wide.trap$tree_species_richness)
+df.all.wide.trap$blk<-as.factor(df.all.wide.trap$block)
+df.all.wide.trap$myc<-as.factor(df.all.wide.trap$mycorrhizal_type)
+df.all.wide.trap$sr<-df.all.wide.trap$tree_species_richness
+df.all.wide.trap$sr_myc<-paste(df.all.wide.trap$sr,df.all.wide.trap$myc,sep="_")#interaction terms
+df.all.wide.trap$myc <- recode_factor(df.all.wide.trap$myc, "AMF" ="AM", "EMF" ="EM", "AMF+EMF" = "AM + EM")
+df.all.wide.trap$block <- as.factor(df.all.wide.trap$block)
+
+
+# 2) long format ####
+df.all.long <- df.all.wide.trap %>% 
+  pivot_longer(cols=c(8:17),
+               names_to="species",
+               values_to="litterfall")
---
+++ 2-code/2-2-analyses/2-2-9-NDMS-species-specific-litterfall.R
@@ -0,0 +1,62 @@
+#---------------------------------------------------------------------
+# MyDiv experiment; Litterfall data March 2023 - February 2024
+# 2024-05-14
+# litter dryweight NMDS on species level
+# by Elisabeth Bönisch (elisabeth.boenisch@idiv.de)
+# updated on 
+
+#============================ Packages ===============================
+
+rm(list=ls())
+
+library(readr)
+library(readxl)
+library(tidyverse)
+library(nlme)
+library(lme4)
+library(lmerTest)
+library(vegan)
+
+#============================ Dataset ===============================
+
+df.all.wide.info <- read.csv("2-1-1-Full-data-wideformat-MyDiv-litter-dryweight.csv")
+
+# 1) average across traps ####
+df.all.wide.mean <- df.all.wide.info %>%
+  dplyr::rename(Ac = "Acer.pseudoplatanus",	
+                Ae = "Aesculus.hippocastanum",
+                Be = "Betula.pendula",	
+                Ca = "Carpinus.betulus",
+                Fa = "Fagus.sylvatica",	
+                Fr = "Fraxinus.excelsior",	
+                Pr = "Prunus.avium",	
+                Qu = "Quercus.petraea", 
+                So = "Sorbus.aucuparia",	
+                Ti = "Tilia.platyphyllos", 
+                cont = "contaminants") %>%
+  dplyr::group_by(plotID, plotName, tree_species_richness, mycorrhizal_type, myc, sr, div, block, blk, month1, month, composition) %>% # removed trap and month
+  dplyr::summarise(Ac_mean = mean(Ac, na.rm = TRUE),	
+                   Ae_mean = mean(Ae, na.rm = TRUE),
+                   Be_mean = mean(Be, na.rm = TRUE),	
+                   Ca_mean = mean(Ca, na.rm = TRUE),
+                   Fa_mean = mean(Fa, na.rm = TRUE),	
+                   Fr_mean = mean(Fr, na.rm = TRUE),	
+                   Pr_mean = mean(Pr, na.rm = TRUE),	
+                   Qu_mean = mean(Qu, na.rm = TRUE), 
+                   So_mean = mean(So, na.rm = TRUE),	
+                   Ti_mean = mean(Ti, na.rm = TRUE), 
+                   cont_mean = mean(cont, na.rm = TRUE))
+
+df.all.wide.mean$div<-as.factor(df.all.wide.mean$tree_species_richness)
+df.all.wide.mean$blk<-as.factor(df.all.wide.mean$block)
+df.all.wide.mean$myc<-as.factor(df.all.wide.mean$mycorrhizal_type)
+df.all.wide.mean$sr<-df.all.wide.mean$tree_species_richness
+df.all.wide.mean$sr_myc<-paste(df.all.wide.mean$sr,df.all.wide.mean$myc,sep="_")#interaction terms
+df.all.wide.mean$myc <- recode_factor(df.all.wide.mean$myc, "AMF" ="AM", "EMF" ="EM", "AMF+EMF" = "AM + EM")
+
+
+# 2) long format ####
+df.all.long <- df.all.wide.mean %>% 
+  pivot_longer(cols=c(13:22),
+               names_to="species",
+               values_to="dryweight")
